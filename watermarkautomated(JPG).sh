8c110d29738537d6b6ce7b22efc9ff25a1ee34f6
Marco <94206717+MarcoCasanova00@users.noreply.github.com>
2022-07-28 02:29:30 +0200

    Add files via upload

---
+++ watermarkautomated(JPG).sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+
+# usage: bash watermark.sh folder
+
+# Change the working directory to the one specified as argument.
+if ! cd "$*"; then
+    echo "error: the folder '$*' doesn't exist."
+    exit
+fi
+
+# Create a directory called "output" into the working directory.
+mkdir output &> /dev/null
+
+# Start counting in 1.
+counter=1
+
+# For each file that ends in .jpg:
+for image in *.jpg; do  
+    convert "$image" \
+            -background transparent \
+            -fill grey \
+            -font ubuntu \
+            -size 380x260 \
+            -pointsize 38 \
+            -gravity southeast \
+            -annotate +0+0 "LE MEME SHOP ETSY" \
+	    -resize 96% \
+            "output/$image"
+
+    # Increment counter by one.
+    ((counter++)) 
+done
